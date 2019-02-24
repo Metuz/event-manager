77a5db002c2aa93290fbc9d0cb41adfcad74db6a
Alejandro Hernández <alejandroohr@gmail.com>
2019-02-24 16:42:43 -0600

    Adding React Router

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+import PropsRoute from './PropsRoute';
+import Event from './Event';
 import Header from './Header';
 import EventList from './EventList';
 
@@ -21,13 +24,27 @@ class Editor extends React.Component {
   render () {
     const { events } = this.state;
     if (events === null) return null;
-    return(
+
+    const { match } = this.props;
+    const eventId = match.params.id;
+    const event = events.find(e => e.id === Number(eventId));
+
+    return (
       <div>
         <Header />
         <EventList events={events} />
+        <PropsRoute path="/events/:id" component={Event} event={event} />
       </div>
     );
   }
 }
+
+Editor.propTypes = {
+  match: PropTypes.shape(),
+};
+
+Editor.defaultProps = {
+  match: undefined,
+};
 
 export default Editor;
